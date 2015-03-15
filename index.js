a669ba6e1416193de9899f683af02937396a008a
Phillip Clark <phillip@flitbit.com>
2015-03-15 09:33:01 -0600

    switched to generating classes; better subclassing

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-module.exports = require('foldero')('./lib', { relative: __dirname });
+'use strict';
+
+var util = require('util');
+
+var $ = {};
+
+function constructErrorClass(it) {
+  if (Array.isArray(it)) {
+    return eval(util.format([ // jshint ignore:line
+      'function %s(message) {',
+      '  $.%s.call(this, message);',
+      '}',
+      'util.inherits(%s, $.%s);',
+      '$["%s"] = %s;'
+    ].join('\n'), it[0], it[1], it[0], it[1], it[0], it[0]));
+  }
+  return eval(util.format([ // jshint ignore:line
+    'function %s(message) {',
+    '  Error.call(this);',
+    '  Error.captureStackTrace(this, this.constructor);',
+
+    '  this.name = this.constructor.name;',
+    '  this.message = message;',
+    '}',
+    'util.inherits(%s, Error);',
+    '$["%s"] = %s;'
+  ].join('\n'), it, it, it, it));
+}
+
+['ArgumentError',
+  'AuthorizationError',
+  ['ForbiddenError', 'AuthorizationError'],
+  ['InauthenticError', 'AuthorizationError'],
+  ['UnauthorizedError', 'AuthorizationError'],
+  'ConflictError',
+  'InvalidOperationError',
+  'NotFoundError',
+  'NotImplementedError',
+  'ServiceUnavailableError',
+  'UnprocessableEntityError'
+].forEach(function(it) {
+  constructErrorClass(it);
+});
+
+$.isInstance = function isInstance(instance, clazz) {
+  var ctor;
+  if (instance instanceof Error &&
+    typeof(instance) === 'object' &&
+    typeof(clazz) === 'function') {
+    ctor = instance.constructor;
+    while (ctor) {
+      if (ctor.name === clazz.name) {
+        return true;
+      }
+      ctor = (ctor.super_) ? ctor.super_ : null;
+    }
+  }
+  return false;
+};
+
+module.exports = $;
--- test/lib-tests.js
+++ test/lib-tests.js
@@ -1,233 +1,231 @@
-(function() {
-  'use strict';
+/*global it: false, describe: false, before: false, after: false, afterEach: false*/
+'use strict';
 
-  var util = require('util'),
-    format = util.format,
-    uuid = require('node-uuid'),
-    expect = require('expect.js'),
-    errors = require('../');
+var util = require('util'),
+  format = util.format,
+  uuid = require('node-uuid'),
+  expect = require('expect.js'),
+  errors = require('../');
 
-  function print(it) {
-    util.log(util.inspect(it, false, 9));
-  }
+function print(it) {
+  util.log(util.inspect(it, false, 9));
+}
 
-  describe('errors', function() {
+describe('errors', function() {
 
-    describe('NotFoundError', function() {
-      var type = 'NotFoundError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
+  describe('NotFoundError', function() {
+    var type = 'NotFoundError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
 
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
     });
 
-    describe('ConflictError', function() {
-      var type = 'ConflictError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
     });
 
-    describe('ArgumentError', function() {
-      var type = 'ArgumentError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
-    });
-
-    describe('ServiceUnavailableError', function() {
-      var type = 'ServiceUnavailableError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
-    });
-
-    describe('AuthorizationError', function() {
-      var type = 'AuthorizationError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
-    });
-
-    describe('InvalidOperationError', function() {
-      var type = 'InvalidOperationError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
-    });
-
-    describe('NotImplementedError', function() {
-      var type = 'NotImplementedError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
-    });
-
-    describe('UnprocessableEntityError', function() {
-      var type = 'UnprocessableEntityError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-
-    });
-
-    describe('UnauthorizedError', function() {
-      var type = 'UnauthorizedError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('is an instanceof AuthorizationError', function() {
-        expect(err).to.be.an(errors.AuthorizationError);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-    });
-
-
-    describe('ForbiddenError', function() {
-      var type = 'ForbiddenError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('is an instanceof AuthorizationError', function() {
-        expect(err).to.be.an(errors.AuthorizationError);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
-    });
-
-    describe('InauthenticError', function() {
-      var type = 'InauthenticError';
-      var msg = format('this is error: %s', uuid.v4());
-      var err = new errors[type](msg);
-
-      it('is an instanceof Error', function() {
-        expect(err).to.be.an(Error);
-      });
-
-      it('conveys error message specified in constructor', function() {
-        expect(err.message).to.be(msg);
-      });
-
-      it('identifies the Error type when converted to a string', function() {
-        expect(err.toString()).to.be(format('%s: %s', type, msg));
-      });
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
     });
 
   });
 
-})();
+  describe('ConflictError', function() {
+    var type = 'ConflictError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+
+  });
+
+  describe('ArgumentError', function() {
+    var type = 'ArgumentError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+
+  });
+
+  describe('ServiceUnavailableError', function() {
+    var type = 'ServiceUnavailableError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+
+  });
+
+  describe('AuthorizationError', function() {
+    var type = 'AuthorizationError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+
+  });
+
+  describe('InvalidOperationError', function() {
+    var type = 'InvalidOperationError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+
+  });
+
+  describe('NotImplementedError', function() {
+    var type = 'NotImplementedError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+
+  });
+
+  describe('UnprocessableEntityError', function() {
+    var type = 'UnprocessableEntityError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+
+  });
+
+  describe('UnauthorizedError', function() {
+    var type = 'UnauthorizedError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('is an instanceof AuthorizationError', function() {
+      expect(err).to.be.an(errors.AuthorizationError);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+  });
+
+
+  describe('ForbiddenError', function() {
+    var type = 'ForbiddenError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('is an instanceof AuthorizationError', function() {
+      expect(err).to.be.an(errors.AuthorizationError);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+  });
+
+  describe('InauthenticError', function() {
+    var type = 'InauthenticError';
+    var msg = format('this is error: %s', uuid.v4());
+    var err = new errors[type](msg);
+
+    it('is an instanceof Error', function() {
+      expect(err).to.be.an(Error);
+    });
+
+    it('conveys error message specified in constructor', function() {
+      expect(err.message).to.be(msg);
+    });
+
+    it('identifies the Error type when converted to a string', function() {
+      expect(err.toString()).to.be(format('%s: %s', type, msg));
+    });
+  });
+
+});
